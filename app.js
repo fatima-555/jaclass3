415366b2bbbf04a68e15343693a7e22ec0b4a6bf
fatima mustufa <fatimamustufa6@gmail.com>
2024-09-12 11:56:25 -0700

    practice JS

---
+++ app.js
@@ -0,0 +1,10 @@
+var userWeight = prompt("Enter your weight");
+var time = prompt("Enter time");
+var gender = prompt("Enter you gender");
+
+if(userWeight <= 200 && time > 6 || genger ){
+    alert("come and tryout!");
+}
+else{
+    alert("better luck next time..");
+}
